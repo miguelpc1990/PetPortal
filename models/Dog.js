bb2f525f04fb9f6dc8cc24df2ced49ff4fcfb213
Maya McKinney <mayamckinney0306@gmail.com>
2022-12-20 21:22:51 -0700

    working on models

---
+++ models/Dog.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dog extends Model {}
+
+Dog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        fixed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        shared: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        medication: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        diet: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        notes: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        }
+    }
+)
