669197c20f6eb74326bf8baca3a48a75f383fb27
marquisegordon <shailomg@gmail.com>
2023-01-04 12:42:05 -0700

    fixed server.js and connections.js

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: process.env.SESS_SECRET,
@@ -19,7 +22,7 @@ const sess = {
         sameSite: 'strict',
     },
     resave: false,
-    saveUtilized: true,
+    saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
@@ -27,7 +30,7 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', exphbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
