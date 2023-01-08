9b805120fcbda929831544b56576d647d8333197
marquisegordon <shailomg@gmail.com>
2023-01-08 16:54:00 -0700

    added post route for owners

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,4 +28,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const ownerData = await Owner.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            dob: req.body.dob,
+            dog_id: req.body.dog_id
+        });
+      
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(ownerData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
